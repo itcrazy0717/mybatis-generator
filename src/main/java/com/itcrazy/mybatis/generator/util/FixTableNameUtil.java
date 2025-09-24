51643780dcc61655815cb99b7442ca506f1494fe
itcrazy0717 <itcrazy0717@gmail.com>
2025-09-24 17:19:55 +0800

    解决跨库扫表生成xml文件异常问题

---
+++ src/main/java/com/itcrazy/mybatis/generator/util/FixTableNameUtil.java
@@ -0,0 +1,100 @@
+package com.itcrazy.mybatis.generator.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author: dengxin.chen
+ * @version: $ FixTableNameUtil.java,v0.1 2025-09-24 17:02 dengxin.chen Exp $
+ * @description:
+ */
+public class FixTableNameUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MybatisCodeGenerateUtil.class);
+
+    /**
+     * 修复数据表名双点问题
+     * by itcrazy0717
+     *
+     * @param xmlFile     xml文件
+     * @param catalogName 数据库名称
+     * @param tableName   表名
+     * @return
+     */
+    public static void fixTableName(File xmlFile, String catalogName, String tableName) {
+        try {
+            // 读取并处理XML内容（替换双点）
+            if (xmlFile.exists()) {
+                String xmlContent = readXmlContent(xmlFile);
+                String fixedContent = fixTableName(xmlContent, catalogName, tableName);
+                // 写入处理后的内容
+                writeXmlContent(xmlFile, fixedContent);
+            }
+        } catch (Exception e) {
+            LOGGER.error("xml文件生成异常", e);
+            throw new RuntimeException("xml文件生成异常");
+        }
+    }
+
+    /**
+     * 读取xml内容
+     * by itcrazy0717
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    private static String readXmlContent(File file) throws Exception {
+        if (!file.exists()) {
+            return null;
+        }
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString();
+    }
+
+    /**
+     * 写入xml内容
+     * by itcrazy0717
+     *
+     * @param file
+     * @param content
+     * @throws IOException
+     */
+    private static void writeXmlContent(File file, String content) throws Exception {
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(content);
+        }
+    }
+
+    /**
+     * 修复表名双点问题
+     * by itcrazy0717
+     *
+     * @param xmlContent
+     * @param catalogName
+     * @param tableName
+     * @return
+     */
+    private static String fixTableName(String xmlContent, String catalogName, String tableName) {
+        if (StringUtils.isBlank(xmlContent)) {
+            return null;
+        }
+        String target = catalogName + ".." + tableName;
+        return xmlContent.replaceAll(target, tableName);
+    }
+}
--- src/main/java/com/itcrazy/mybatis/generator/util/MybatisCodeGenerateUtil.java
+++ src/main/java/com/itcrazy/mybatis/generator/util/MybatisCodeGenerateUtil.java
@@ -1,5 +1,6 @@
 package com.itcrazy.mybatis.generator.util;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -44,38 +45,38 @@ public class MybatisCodeGenerateUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MybatisCodeGenerateUtil.class);
 
-	/**
-	 * 代码生成配置
-	 */
-	private static MybatisGeneratorTemplate generateConfig;
+    /**
+     * 代码生成配置
+     */
+    private static MybatisGeneratorTemplate generateConfig;
 
-	/**
-	 * 数据库配置
-	 */
-	private static DatabaseConnectionConfig selectedDatabaseConfig;
+    /**
+     * 数据库配置
+     */
+    private static DatabaseConnectionConfig selectedDatabaseConfig;
 
-	/**
-	 * 回调处理
-	 */
+    /**
+     * 回调处理
+     */
     private static ProgressCallback progressCallback;
 
-	/**
-	 * 忽略的列
-	 */
+    /**
+     * 忽略的列
+     */
     private static List<IgnoredColumn> ignoredColumns;
 
-	/**
-	 * 覆盖的列
-	 */
+    /**
+     * 覆盖的列
+     */
     private static List<ColumnOverride> columnOverrides;
 
-	/**
-	 * 生成代码
-	 * by itcrazy0717
-	 *
-	 * @throws Exception
-	 */
-	public static void generateCode() throws Exception {
+    /**
+     * 生成代码
+     * by itcrazy0717
+     *
+     * @throws Exception
+     */
+    public static void generateCode() throws Exception {
         Configuration configuration = new Configuration();
         Context context = new Context(ModelType.CONDITIONAL);
         configuration.addContext(context);
@@ -90,15 +91,15 @@ public class MybatisCodeGenerateUtil {
         tableConfig.setDomainObjectName(generateConfig.getDomainObjectName());
         tableConfig.setCatalog(selectedDatabaseConfig.getSchemaName());
 
-		// 针对postgresql单独配置
-		if (StringUtils.equals(DataBaseTypeEnum.PostgreSQL.getDriverClass(), DataBaseTypeEnum.valueOf(dataBaseType).getDriverClass())) {
-			tableConfig.setDelimitIdentifiers(true);
-		}
+        // 针对postgresql单独配置
+        if (StringUtils.equals(DataBaseTypeEnum.PostgreSQL.getDriverClass(), DataBaseTypeEnum.valueOf(dataBaseType).getDriverClass())) {
+            tableConfig.setDelimitIdentifiers(true);
+        }
 
         // 添加GeneratedKey主键生成，用于insert的时候返回主键
-		// 以上只在MySql下进行过测试
-		if (generateConfig.isInsertReturnPrimaryKey()
-			&& StringUtils.isNotBlank(generateConfig.getPrimaryKeyField())) {
+        // 以上只在MySql下进行过测试
+        if (generateConfig.isInsertReturnPrimaryKey()
+            && StringUtils.isNotBlank(generateConfig.getPrimaryKeyField())) {
             String dbType = dataBaseType;
             if (StringUtils.equals(DataBaseTypeEnum.MySQL.name(), dbType)) {
                 dbType = "JDBC";
@@ -113,14 +114,14 @@ public class MybatisCodeGenerateUtil {
             tableConfig.setGeneratedKey(new GeneratedKey(generateConfig.getPrimaryKeyField(), dbType, true, null));
         }
 
-	    if (StringUtils.isNotBlank(generateConfig.getMapperName())) {
-		    tableConfig.setMapperName(generateConfig.getMapperName());
-	    }
+        if (StringUtils.isNotBlank(generateConfig.getMapperName())) {
+            tableConfig.setMapperName(generateConfig.getMapperName());
+        }
         // add ignore columns
         if (CollectionUtils.isNotEmpty(ignoredColumns)) {
             ignoredColumns.forEach(tableConfig::addIgnoredColumn);
         }
-        if (CollectionUtils.isNotEmpty(columnOverrides )) {
+        if (CollectionUtils.isNotEmpty(columnOverrides)) {
             columnOverrides.forEach(tableConfig::addColumnOverride);
         }
         JDBCConnectionConfiguration jdbcConfig = new JDBCConnectionConfiguration();
@@ -128,49 +129,49 @@ public class MybatisCodeGenerateUtil {
         jdbcConfig.setConnectionURL(DataBaseUtil.buildConnectionUrlWithSchema(selectedDatabaseConfig));
         jdbcConfig.setUserId(selectedDatabaseConfig.getUserName());
         jdbcConfig.setPassword(selectedDatabaseConfig.getPassword());
+
         // 实体类路径配置
         JavaModelGeneratorConfiguration modelConfig = new JavaModelGeneratorConfiguration();
         modelConfig.setTargetPackage(generateConfig.getModelPackage());
         modelConfig.setTargetProject(generateConfig.getProjectFolder() + "/" + generateConfig.getModelAndDaoInterfacePackageTargetFolder());
 
-	    // DAO接口文件路径配置
-	    JavaClientGeneratorConfiguration daoConfig = new JavaClientGeneratorConfiguration();
-	    daoConfig.setConfigurationType("XMLMAPPER");
-	    daoConfig.setTargetPackage(generateConfig.getDaoPackage());
-	    daoConfig.setTargetProject(generateConfig.getProjectFolder() + "/" + generateConfig.getModelAndDaoInterfacePackageTargetFolder());
+        // DAO接口文件路径配置
+        JavaClientGeneratorConfiguration daoConfig = new JavaClientGeneratorConfiguration();
+        daoConfig.setConfigurationType("XMLMAPPER");
+        daoConfig.setTargetPackage(generateConfig.getDaoPackage());
+        daoConfig.setTargetProject(generateConfig.getProjectFolder() + "/" + generateConfig.getModelAndDaoInterfacePackageTargetFolder());
 
         // Mapper文件路径配置
         SqlMapGeneratorConfiguration mapperConfig = new SqlMapGeneratorConfiguration();
         mapperConfig.setTargetPackage(generateConfig.getMapperXMLPackage());
         mapperConfig.setTargetProject(generateConfig.getProjectFolder() + "/" + generateConfig.getMapperXMLTargetFolder());
 
-	    // 设置param对象包路径，单独命名为xxx.parm,便于管理
-	    String paramPackage = generateConfig.getParamModelPackage();
-	    if (StringUtils.isBlank(paramPackage)) {
-		    // 未自定义param包路径，则默认使用实体包路径，兜底
-		    String modelPackage = modelConfig.getTargetPackage();
-		    paramPackage = modelPackage.substring(0, modelPackage.lastIndexOf(".")) + ".param";
-	    }
+        // 设置param对象包路径，单独命名为xxx.parm,便于管理
+        String paramPackage = generateConfig.getParamModelPackage();
+        if (StringUtils.isBlank(paramPackage)) {
+            // 未自定义param包路径，则默认使用实体包路径，兜底
+            String modelPackage = modelConfig.getTargetPackage();
+            paramPackage = modelPackage.substring(0, modelPackage.lastIndexOf(".")) + ".param";
+        }
 
         context.setId("myid");
         context.addTableConfiguration(tableConfig);
-        context.setJdbcConnectionConfiguration(jdbcConfig);
         context.setJdbcConnectionConfiguration(jdbcConfig);
         context.setJavaModelGeneratorConfiguration(modelConfig);
         context.setSqlMapGeneratorConfiguration(mapperConfig);
         context.setJavaClientGeneratorConfiguration(daoConfig);
         // 注释
         CommentGeneratorConfiguration commentConfig = new CommentGeneratorConfiguration();
-	    commentConfig.setConfigurationType(CustomCommentGenerator.class.getName());
-	    commentConfig.addProperty("columnRemarks", "true");
-	    // commentConfig.addProperty("annotations", "true");
+        commentConfig.setConfigurationType(CustomCommentGenerator.class.getName());
+        commentConfig.addProperty("columnRemarks", "true");
+        // commentConfig.addProperty("annotations", "true");
 
-	    context.setCommentGeneratorConfiguration(commentConfig);
+        context.setCommentGeneratorConfiguration(commentConfig);
 
-		// 增加自定义插件
-	    addCustomPlugins(context, paramPackage);
+        // 增加自定义插件
+        addCustomPlugins(context, paramPackage);
 
-	    context.setTargetRuntime("MyBatis3");
+        context.setTargetRuntime("MyBatis3");
 
         List<String> warnings = new ArrayList<>();
         Set<String> fullyqualifiedTables = new HashSet<>();
@@ -178,88 +179,104 @@ public class MybatisCodeGenerateUtil {
         ShellCallback shellCallback = new DefaultShellCallback(true);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(configuration, shellCallback, warnings);
         myBatisGenerator.generate(progressCallback, contexts, fullyqualifiedTables);
+        FixTableNameUtil.fixTableName(buildXmlFile(), selectedDatabaseConfig.getSchemaName(), generateConfig.getTableName());
     }
 
-	/**
-	 * 增加自定义插件
-	 * by itcrazy0717
-	 *
-	 * @param context
-	 * @param paramPackage parm参数包路径
-	 */
-	private static void addCustomPlugins(Context context, String paramPackage) {
-		// 默认设置TINYINT->Boolean类型
-		JavaTypeResolverConfiguration typeResolverConfiguration = new JavaTypeResolverConfiguration();
-		typeResolverConfiguration.setConfigurationType(TinyIntTypeResolver.class.getName());
-		context.setJavaTypeResolverConfiguration(typeResolverConfiguration);
-		/**
-		 // 序列化插件
-		 PluginConfiguration serializablePlugin = new PluginConfiguration();
-		 serializablePlugin.addProperty("type", "org.mybatis.generator.plugins.SerializablePlugin");
-		 serializablePlugin.setConfigurationType("org.mybatis.generator.plugins.SerializablePlugin");
-		 context.addPluginConfiguration(serializablePlugin);
-		 */
+    /**
+     * 增加自定义插件
+     * by itcrazy0717
+     *
+     * @param context
+     * @param paramPackage parm参数包路径
+     */
+    private static void addCustomPlugins(Context context, String paramPackage) {
+        // 默认设置TINYINT->Boolean类型
+        JavaTypeResolverConfiguration typeResolverConfiguration = new JavaTypeResolverConfiguration();
+        typeResolverConfiguration.setConfigurationType(TinyIntTypeResolver.class.getName());
+        context.setJavaTypeResolverConfiguration(typeResolverConfiguration);
+        /**
+         // 序列化插件
+         PluginConfiguration serializablePlugin = new PluginConfiguration();
+         serializablePlugin.addProperty("type", "org.mybatis.generator.plugins.SerializablePlugin");
+         serializablePlugin.setConfigurationType("org.mybatis.generator.plugins.SerializablePlugin");
+         context.addPluginConfiguration(serializablePlugin);
+         */
 
-		// toString插件
-		PluginConfiguration toStringPlugin = new PluginConfiguration();
-		toStringPlugin.addProperty("type", "org.mybatis.generator.plugins.ToStringPlugin");
-		toStringPlugin.setConfigurationType("org.mybatis.generator.plugins.ToStringPlugin");
-		context.addPluginConfiguration(toStringPlugin);
-		// 分页插件
-		if (DataBaseTypeEnum.MySQL.name().equals(selectedDatabaseConfig.getDataBaseType()) || DataBaseTypeEnum.PostgreSQL.name().equals(selectedDatabaseConfig.getDataBaseType())) {
-			PluginConfiguration pagePlugin = new PluginConfiguration();
-			pagePlugin.addProperty("", "com.itcrazy.mybatis.generator.plugins.PagePlugin");
-			pagePlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.PagePlugin");
-			context.addPluginConfiguration(pagePlugin);
-		}
-		// 覆写xml文件插件
-		PluginConfiguration overWiriteXmlPlugin = new PluginConfiguration();
-		overWiriteXmlPlugin.addProperty("type", "org.mybatis.generator.plugins.UnmergeableXmlMappersPlugin");
-		overWiriteXmlPlugin.setConfigurationType("org.mybatis.generator.plugins.UnmergeableXmlMappersPlugin");
-		context.addPluginConfiguration(overWiriteXmlPlugin);
-		// 替换example内容插件
-		PluginConfiguration replaeceExampleContentPlugin = new PluginConfiguration();
-		replaeceExampleContentPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.ReplaceExampleContentPlugin");
-		replaeceExampleContentPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.ReplaceExampleContentPlugin");
-		replaeceExampleContentPlugin.addProperty("searchString", "Example");
-		replaeceExampleContentPlugin.addProperty("replaceString", "Param");
-		replaeceExampleContentPlugin.addProperty("simpleMethod", "True");
-		replaeceExampleContentPlugin.addProperty("paramPackage", paramPackage);
-		context.addPluginConfiguration(replaeceExampleContentPlugin);
-		// 方法注释插件
-		PluginConfiguration addMethodComentPlugin = new PluginConfiguration();
-		addMethodComentPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.AddMethodCommentPlugin");
-		addMethodComentPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.AddMethodCommentPlugin");
-		context.addPluginConfiguration(addMethodComentPlugin);
-		// 批量插入插件
-		PluginConfiguration batchInsertPlugin = new PluginConfiguration();
-		batchInsertPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.BatchInsertPlugin");
-		batchInsertPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.BatchInsertPlugin");
-		context.addPluginConfiguration(batchInsertPlugin);
-		// 排序插件
-		PluginConfiguration sortPlugin = new PluginConfiguration();
-		sortPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.SortPlugin");
-		sortPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.SortPlugin");
-		context.addPluginConfiguration(sortPlugin);
-	}
+        // toString插件
+        PluginConfiguration toStringPlugin = new PluginConfiguration();
+        toStringPlugin.addProperty("type", "org.mybatis.generator.plugins.ToStringPlugin");
+        toStringPlugin.setConfigurationType("org.mybatis.generator.plugins.ToStringPlugin");
+        context.addPluginConfiguration(toStringPlugin);
+        // 分页插件
+        if (DataBaseTypeEnum.MySQL.name().equals(selectedDatabaseConfig.getDataBaseType()) || DataBaseTypeEnum.PostgreSQL.name().equals(selectedDatabaseConfig.getDataBaseType())) {
+            PluginConfiguration pagePlugin = new PluginConfiguration();
+            pagePlugin.addProperty("", "com.itcrazy.mybatis.generator.plugins.PagePlugin");
+            pagePlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.PagePlugin");
+            context.addPluginConfiguration(pagePlugin);
+        }
+        // 覆写xml文件插件
+        PluginConfiguration overWiriteXmlPlugin = new PluginConfiguration();
+        overWiriteXmlPlugin.addProperty("type", "org.mybatis.generator.plugins.UnmergeableXmlMappersPlugin");
+        overWiriteXmlPlugin.setConfigurationType("org.mybatis.generator.plugins.UnmergeableXmlMappersPlugin");
+        context.addPluginConfiguration(overWiriteXmlPlugin);
+        // 替换example内容插件
+        PluginConfiguration replaeceExampleContentPlugin = new PluginConfiguration();
+        replaeceExampleContentPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.ReplaceExampleContentPlugin");
+        replaeceExampleContentPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.ReplaceExampleContentPlugin");
+        replaeceExampleContentPlugin.addProperty("searchString", "Example");
+        replaeceExampleContentPlugin.addProperty("replaceString", "Param");
+        replaeceExampleContentPlugin.addProperty("simpleMethod", "True");
+        replaeceExampleContentPlugin.addProperty("paramPackage", paramPackage);
+        context.addPluginConfiguration(replaeceExampleContentPlugin);
+        // 方法注释插件
+        PluginConfiguration addMethodComentPlugin = new PluginConfiguration();
+        addMethodComentPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.AddMethodCommentPlugin");
+        addMethodComentPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.AddMethodCommentPlugin");
+        context.addPluginConfiguration(addMethodComentPlugin);
+        // 批量插入插件
+        PluginConfiguration batchInsertPlugin = new PluginConfiguration();
+        batchInsertPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.BatchInsertPlugin");
+        batchInsertPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.BatchInsertPlugin");
+        context.addPluginConfiguration(batchInsertPlugin);
+        // 排序插件
+        PluginConfiguration sortPlugin = new PluginConfiguration();
+        sortPlugin.addProperty("type", "com.itcrazy.mybatis.generator.plugins.SortPlugin");
+        sortPlugin.setConfigurationType("com.itcrazy.mybatis.generator.plugins.SortPlugin");
+        context.addPluginConfiguration(sortPlugin);
 
-	/**
-	 * 导入配置
-	 * by itcrazy0717
-	 *
-	 * @param generateConfig
-	 * @param databaseConfig
-	 * @param progressCallback
-	 * @param ignoredColumns
-	 * @param columnOverrides
-	 */
-	public static void loadConfig(MybatisGeneratorTemplate generateConfig, DatabaseConnectionConfig databaseConfig,
-								  ProgressCallback progressCallback, List<IgnoredColumn> ignoredColumns,
-								  List<ColumnOverride> columnOverrides) {
-		MybatisCodeGenerateUtil.generateConfig = generateConfig;
-		MybatisCodeGenerateUtil.selectedDatabaseConfig = databaseConfig;
-		MybatisCodeGenerateUtil.progressCallback = progressCallback;
-		MybatisCodeGenerateUtil.ignoredColumns = ignoredColumns;
-		MybatisCodeGenerateUtil.columnOverrides = columnOverrides;
-	}
+    }
+
+    /**
+     * 导入配置
+     * by itcrazy0717
+     *
+     * @param generateConfig
+     * @param databaseConfig
+     * @param progressCallback
+     * @param ignoredColumns
+     * @param columnOverrides
+     */
+    public static void loadConfig(MybatisGeneratorTemplate generateConfig, DatabaseConnectionConfig databaseConfig,
+                                  ProgressCallback progressCallback, List<IgnoredColumn> ignoredColumns,
+                                  List<ColumnOverride> columnOverrides) {
+        MybatisCodeGenerateUtil.generateConfig = generateConfig;
+        MybatisCodeGenerateUtil.selectedDatabaseConfig = databaseConfig;
+        MybatisCodeGenerateUtil.progressCallback = progressCallback;
+        MybatisCodeGenerateUtil.ignoredColumns = ignoredColumns;
+        MybatisCodeGenerateUtil.columnOverrides = columnOverrides;
+    }
+
+    /**
+     * 构建xml文件
+     * by itcrazy0717
+     *
+     * @return
+     */
+    private static File buildXmlFile() {
+        // 构建目录路径
+        String dirPath = generateConfig.getProjectFolder() + "/" + generateConfig.getMapperXMLTargetFolder() + "/" + generateConfig.getMapperXMLPackage().replace(".", "/");
+        File directory = new File(dirPath);
+        String fileName = generateConfig.getMapperName() + ".xml";
+        return new File(directory, fileName);
+    }
 }
